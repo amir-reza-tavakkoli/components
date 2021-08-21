50b323834f58ee77c8732d8345f52dde2c45cfc5
amir-reza-tavakkoli <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2021-08-21 17:03:58 +0430

    vue the profile page

---
+++ profile-page/main.js
@@ -0,0 +1,11 @@
+import { createApp } from '../packages/vue.js'
+import { ProfilePage } from './profile-page.js'
+
+let app = createApp({
+    components: { ProfilePage },
+    template: `
+        <ProfilePage></ProfilePage>
+    `,
+})
+
+app.mount('#app')
