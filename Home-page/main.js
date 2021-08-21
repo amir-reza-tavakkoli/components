a0b7544bae4510b2aac3d170a8be22584528cbf1
amir-reza-tavakkoli <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2021-08-21 17:00:41 +0430

    Add files via upload

---
+++ Home-page/main.js
@@ -0,0 +1,11 @@
+import { createApp } from '../packages/vue.js'
+import { MainPage } from './main-page.js'
+
+let app = createApp({
+    components: { MainPage },
+    template: `
+        <MainPage></MainPage>
+    `,
+})
+
+app.mount('#app')
